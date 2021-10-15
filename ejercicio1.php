e660e62c145e06e902a7391d564523c726842e88
root <manuel.ramos.illana.al@iespoligonosur.org>
2021-10-15 14:29:11 +0200

    haciendo ejer1

---
+++ ejercicio1.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php   
+echo '<table border="1">';
+$file = fopen("prueba.txt","a+");
+
+
+//fwrite($file, "manuel ramos");
+
+while(!feof($file))
+{
+
+echo "<tr><td>". fgets($file). "</td><td>";
+
+
+}
+
+str_getcsv(
+    
+    string $delimiter = ",",  
+);
+
+echo '</table>';
+
+
+fclose($file);
+?>
+
+?>
+
+
+    
+</body>
+</html>
